62391f69d38d63dd60f1694ce338577a6e11ed6b
AndreiaSandovalGomes <gomesandreia27@outlook.com>
2018-11-09 14:20:17 -0200

    wadson

---
+++ data.js
@@ -0,0 +1,114 @@
+const companies = [
+  {
+    'name': "Accenture",
+    'logo': "images/Accenture.png",
+    'recommendation': 5,
+    'salary': 5,
+    'career': 5,
+    "total average ": 5
+  },
+  {
+    'name': "Avanade",
+    'logo': "images/share-avanade-logo.jpg",
+    'recommendation': 5,
+    'salary': 5,
+    'career': 5,
+    "total average ": 5
+  },
+  {
+    'name': "Bradesco",
+    'logo': "images/Bradesco.png",
+    'recommendation': 5,
+    'salary': 5,
+    'career': 5,
+    "total average ": 5
+  },
+  {
+    'name': "Creditas",
+    'logo': "images/creditas.png",
+    'recommendation': 5,
+    'salary': 5,
+    'career': 5,
+    "total average ": 5
+  },
+  {
+    'name': "Dasa",
+    'logo': "images/dasa.png",
+    'recommendation': 5,
+    'salary': 5,
+    'career': 5,
+    "total average ": 5
+  },
+  {
+    'name': "Drumwave",
+    'logo': "images/logo-drumwave.png",
+    'recommendation': 5,
+    'salary': 5,
+    'career': 5,
+    "total average ": 5
+  },
+  {
+    'name': "FCamara",
+    'logo': "images/logotipo-horizontal-fcamara-2015-e1455123821994.png",
+    'recommendation': 5,
+    'salary': 5,
+    'career': 5,
+    "total average ": 5
+  },
+  {
+    'name': "Hardwork",
+    'logo': "images/hardwork.jpg",
+    'recommendation': 1,
+    'salary': 2,
+    'career': 1,
+    "total average ": 1.5
+  },
+  {
+    'name': "GFT",
+    'logo': "images/gft.jpg",
+    'recommendation': 5,
+    'salary': 5,
+    'career': 5,
+    'total average': 5
+  },
+  {
+    'name': "IBM",
+    'logo': 'images/IBM.png',
+    'recommendation': 5,
+    'salary': 5,
+    'career': 5,
+    "total average ": 5
+  },
+  {
+    'name': "Loggi",
+    'logo': "images/Loggi.png",
+    'recommendation': 5,
+    'salary': 5,
+    'career': 5,
+    "total average ": 5
+  },
+  {
+    'name': "PowerMan",
+    'logo': "images/powerman_logo_1920x1280.png",
+    'recommendation': 0,
+    'salary': 1,
+    'career': 0,
+    "total average ": 0.5
+  },
+  {
+    'name': "Stefanini",
+    'logo': "images/Stafanini.png",
+    'recommendation': 5,
+    'salary': 5,
+    'career': 5,
+    "total average ": 5
+  },
+  {
+    'name': "Tivit",
+    'logo': "images/logo_tivit.png",
+    'recommendation': 5,
+    'salary': 5,
+    'career': 5,
+    "total average ": 5
+  }
+];
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-console */
+var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+
 const express = require('express');
 const ajaxResquest = require('request');
 const bodyParser = require('body-parser');
@@ -6,9 +8,22 @@ const path = require('path');
 const PORT = 8080;
 const app = express();
 const toneUsername= "apikey";
-const tonePassword= "...suachave...";
+const tonePassword= "DXnguc0TPIbu5laENNgnNUoOaAUXgb1zErUN_KqMHqGc";
 const toneUrl = "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21";
 
+var toneAnalyzer = new ToneAnalyzerV3({
+  version: '2017-09-21',
+  iam_apikey: 'DXnguc0TPIbu5laENNgnNUoOaAUXgb1zErUN_KqMHqGc',
+  url: 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21'
+});
+
+var LanguageTranslatorV3 = require('watson-developer-cloud/language-translator/v3');
+
+var languageTranslator = new LanguageTranslatorV3({
+    version: '2018-05-01',
+    iam_apikey: 'oM_wgsnP-pbQj8_hM5oUZJQH_KwHT657WhlO69Eler0I',
+    url: 'https://gateway.watsonplatform.net/language-translator/api'
+  });
 app.use("/dist", express.static(__dirname + '/dist'));
 
 app.use(bodyParser.json());
@@ -18,19 +33,52 @@ app.get('/', function (request, response) {
 });
 
 app.get('/toner', function (request, response) {
-  ajaxResquest.post({
-    url: toneUrl,
-    json: {
-      text: request.query.data,
-    },
-    auth: {
-      user: toneUsername,
-      pass: tonePassword
+
+  var parameters = {
+    text: request.query.data,
+    model: 'en-us',
+    source: 'pt-br',
+    target: 'en-us'
+  };
+  languageTranslator.translate(
+    parameters,
+    function(error, response) {
+      if (error)
+      console.log(error)
+      else{
+        //console.log(JSON.stringify(response, null, 2));
+        const texto = JSON.stringify(response.translations[0].translation, null, 2);
+        analyse(texto);
+      }
+
     }
-  }, function (err, res, body){
-    response.json(body); 
-  });
-});
+  );
+    function analyse(texto){
+      var toneParams = {
+        tone_input: { 'text': texto },
+        content_type: 'application/json',
+        accept_language: 'pt-br'
+      };
+
+      toneAnalyzer.tone(toneParams, function(error, toneAnalysis){
+        if(error){
+          console.log(error)
+        }
+        else{
+          let emojis = []
+          const tons = toneAnalysis.document_tone.tones;
+          //console.log(JSON.stringify(toneAnalysis, null, 2));
+          let i;
+          /*for(i=0;i<tons.length; i++){
+            if(tons[i].tone_id == )
+          }*/
+          response.send(toneAnalysis.document_tone.tones);
+        }
+      })
+    }
+
+  })
 
 app.listen(PORT);
+
 console.log("server started on PORT " + PORT);
